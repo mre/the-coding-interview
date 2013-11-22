e0d6f35234551b1f7a2eebf1cfbf50dbede0806a
Matthias Endler <matthias-endler@gmx.net>
2013-11-22 15:23:12 +0100

    A solution for csv-parsing

--- problems/csv-parsing/csv-parsing.py
+++ problems/csv-parsing/csv-parsing.py
@@ -1,13 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 
-def to_csv(tokens):
-    csv = []
-    for t in tokens.split("'"):
-        try:
-            val = int(t)
-            csv.append(val)
-        except:
-            csv.append(str(t))
-    return csv
+def to_csv(s):
+  results = []
+  str_token = []
+  for t in s.split(","):
+    try:
+      # Int?
+      results.append(int(t))
+    except:
+      # String
+      str_token.append(t)
+      if t[-1] == '"':
+        result = ",".join(str_token)
+        # Remove unneeded apostrophes
+        results.append(result[1:-1])
+        str_token = []
+  return results
 
 print to_csv('2,6,3,2,5')
 print to_csv('"pears","apples","walnuts","grapes","cheese,cake"')
+print to_csv('1,"Que?","Kay?",2,"Si.","Sea? Kay, sea?","No, no, no. Que...  ‘what’.", \
+             234,"Kay Watt?","Si, que ‘what’.","C.K.  Watt?",3,"Yes!","comma,comma, comma , :)"')
