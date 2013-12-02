b657d95115c97fa71da9a0b7d5d645e2fb2ded59
Matthias Endler <matthias-endler@gmx.net>
2013-12-02 09:18:12 +0100

    String permutations

--- problems/string-format/string-format.py
+++ problems/string-format/string-format.py
@@ -5,15 +5,13 @@ def formatter_brute(string, *args):
         string = re.sub("\{" + str(i) + "\}", argument, string)
     return string
 
-"""
 def formatter(string, *args):
-    tags = re.findall( r'\{(\d)\}', string)
-    for t in tags:
-        print t
+    #tags = re.findall( r'\{(\d)\}', string)
+    for word in string.split():
+        print word
     #for i, argument in enumerate(args):
     #    string = re.sub("\{" + str(i) + "\}", argument, string)
     #return string
-"""
 
 print formatter('Hello {0} {1}', 'Mr.', 'X')
 print formatter('{1}_{0}', '{1}', '{0}')
