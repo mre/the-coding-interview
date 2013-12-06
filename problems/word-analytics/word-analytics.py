55581fee30209f9c6b087e4e082790c15547b677
Matthias Endler <matthias-endler@gmx.net>
2013-12-06 15:19:30 +0100

    Some quick thoughs on word-analytics

--- problems/word-analytics/word-analytics.py
+++ problems/word-analytics/word-analytics.py
@@ -1,4 +1,4 @@
-from string import lower
+from string import upper
 import sys
 
 letters = "abcdefghijklmnopqrstuvwxyz"
@@ -7,11 +7,25 @@ digits  = "0123456789"
 text = sys.stdin.read()
 words = text.split()
 
-num_letters = 0
+num_letters = num_symbols = 0
+letter_count = [0]*len(letters)
+
 for word in words:
-  word = lower(word)
-  print ">>" + word + "<<"
-  num_letters += 1
+  word = upper(word)
+  for letter in word:
+    if letter in letters + digits:
+      num_letters += 1
+      try:
+        # Find most common letters
+        letter_count[letters.index(letter)] += 1
+      except:
+        pass
+    else:
+      if letter != " ":
+        num_symbols += 1
+top_three_letters = sorted(letter_count)[-3:]
 
 print len(words), "words"
-print letters, "letters"
+print num_letters, "letters"
+print num_symbols, "symbols"
+print "Top three most common letters:", ", ".join([letters[letter_count.index(top)] for top in top_three_letters])
