2ac89a02c06a82cfb540f9c9041ef5c4dbe9ea5e
Matthias Endler <matthias-endler@gmx.net>
2013-12-23 10:09:35 +0100

    Generic input. Yeah

--- problems/codejam/2013/quali/Tic-Tac-Toe-Tomek/tic-tac-toe-tomek.py
+++ problems/codejam/2013/quali/Tic-Tac-Toe-Tomek/tic-tac-toe-tomek.py
@@ -1,5 +1,18 @@
-infile = open("in.txt").read()
+import sys, time
+
+def get_data():
+    numcases = int(sys.stdin.readline())
+    for case in range(1,numcases+1):
+        data = []
+        while True:
+            line = sys.stdin.readline().strip()
+            if line:
+                data.append(line)
+            else:
+                break
+        yield (case, data)
 
 
-nr_testcases = infile.split("\n")
-print nr_testcases
+def main():
+    for case, data in get_data():
+        #blub
