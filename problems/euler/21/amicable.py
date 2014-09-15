b9418199c3a75111d94f2990107082ef46d6e9e3
Matthias Endler <matthias-endler@gmx.net>
2014-09-15 23:17:28 +0200

    Solution to 21

--- problems/euler/21/amicable.py
+++ problems/euler/21/amicable.py
@@ -5,28 +5,44 @@ from prime_sieve import primes_upto
 def factorize(n, primes):
     factors = []
     for p in primes:
-        if p*p > n:
+        if p > n:
             break
         i = 0
-        while n % p:
+        while n % p == 0:
             n //= p
             i += 1
         if i > 0:
             factors.append((p,i))
-    if n > 1: factors.append((n,1))
+    # Include n itself?
+    #if n > 1: factors.append((n,1))
     return factors
 
 def divisors(n, primes):
     div = [1]
-    for (p,i) in factorize(n):
-        div = [d * p**e for d in div for e in range(i+1)]
+    factors = factorize(n, primes)
+    for p,i in factors:
+        div = [d * p**e for d in div for e in range(i+1) if d* p**e != n]
     return div
 
+def find_amicable(d):
+    amicable = set()
+    for a,b in d.iteritems():
+        try:
+            if d[b] == a:
+                if a != b:
+                    amicable.update([a, b])
+        except KeyError:
+            pass
+    return amicable
+
 def main():
-    MAX = 100
+    MAX = 10001
     primes = [p for p in primes_upto(MAX)]
+    d = {}
     for n in range(1,MAX):
-        print n, divisors(n, primes)
+        d[n] = sum(divisors(n, primes))
+    amicable = find_amicable(d)
+    print sum(amicable)
 
 if __name__ == "__main__":
     main()
--- problems/euler/21/prime_sieve.py
+++ problems/euler/21/prime_sieve.py
@@ -1,12 +1,12 @@
 def primes_upto(n):
     # Our well known sieve...
     multiples = set()
+    yield 2
     # Optimization: Add the even numbers
     multiples.update(range(4, n, 2))
 
     for p in range(3,n,2):
-        if i not in multiples:
+        if p not in multiples:
             # Found a prime!
-            yield i
-            multiples.update(range(i*2, n, i))
-        i = i + 1
+            yield p
+            multiples.update(range(p*2, n, p))
