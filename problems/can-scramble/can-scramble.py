c63c27ecdecfcc51c0b4577ff2ce5830abac0333
Matthias Endler <matthias.endler@trivago.com>
2016-09-07 15:02:11 +0200

    Simplify can-scramble for Python

--- problems/can-scramble/can-scramble.py
+++ problems/can-scramble/can-scramble.py
@@ -3,12 +3,8 @@ from collections import Counter
 def can_scramble(source, dest):
   if len(source) != len(dest):
     return False
-  counter_source = Counter(source)
-  counter_dest = Counter(dest)
-  for k, v in counter_dest.items():
-    if counter_source[k] != v:
-      return False
-  return True
+  return Counter(source) == Counter(dest)
+
 
 assert(can_scramble("abc", "cba") == True)
 assert(can_scramble("abc", "ccc") == False)
