f4e7367c2d8b9ed1b78085be13c4e85928a2525c
diego <diego@busuu.com>
2017-09-20 14:10:30 +0100

    edge cases

--- problems/atbash/atbash.php
+++ problems/atbash/atbash.php
@@ -1,7 +1,10 @@
 <?php
 
 function atbash(string $s) {
-    $x = "";
+    if (ord(strtolower($s)) < ord('a') || ord(strtolower($s)) > ord('z')) {
+        return $s;
+    }
+    $x = '';
     foreach (str_split(strtolower($s)) as $c) {
         $x .= chr(ord('z') - (ord($c) - ord('a')));
     }
@@ -10,3 +13,6 @@ function atbash(string $s) {
 
 assert(atbash('abcdefghijklmnopqrstuvwxyz') === 'zyxwvutsrqponmlkjihgfedcba');
 assert(atbash('old') === 'low');
+assert(atbash('LOW') === 'old');
+assert(atbash('') === '');
+assert(atbash('?') === '?');
