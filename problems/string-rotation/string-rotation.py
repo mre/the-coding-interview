afd6dcbe5cd0792286e60abfa8109a442dbf7c13
Manuel Gutierrez <mgp.xr09@gmail.com>
2017-10-24 15:38:57 -0400

    Smarter algorithm for string rotation with python

--- problems/string-rotation/string-rotation.py
+++ problems/string-rotation/string-rotation.py
@@ -9,12 +9,18 @@ def is_rotation_brute(string, candidate, stop=""):
     rot = candidate[1:] + candidate[0]
     return is_rotation(string, rot, stop)
 
+
 def is_rotation(string, candidate):
     for rotation in range(len(candidate)):
         if string == candidate:
             return True
         candidate = candidate[1:] + candidate[0]
     return False
+
+
+def is_rotation2(original, candidate):
+    return len(original) == len(candidate) and candidate in original*2
+
 
 print is_rotation("ABCD", "ABCD")
 print is_rotation("ABCD", "BCDA")
