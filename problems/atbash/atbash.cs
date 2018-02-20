8c35f3e29906a0a66caa875ec8241a03357880be
spiros nm <pipis.nm@hotmail.com>
2018-02-20 21:57:42 +0200

    atbash c# solution

---
+++ problems/atbash/atbash.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace Testing
+{
+    class Program
+    {
+
+        const string letters = "abcdefghijklmnopqrstuvwxyz";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(_Cipher("irk"));
+            Console.WriteLine(_Cipher("low"));
+            Console.WriteLine(_Cipher("hob"));
+            Console.WriteLine(_Cipher("hold"));
+            Console.ReadKey();
+
+        }
+
+        private static string _Cipher(string text)
+        {
+            string cipheredText = "";
+            int index;
+            foreach (var c in text)
+            {
+                index = letters.IndexOf(c);
+                cipheredText += letters.Substring(25 - (index), 1);
+            }
+            return cipheredText;
+        }
+
+    }
+}
