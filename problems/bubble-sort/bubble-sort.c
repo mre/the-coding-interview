30060b12cb01289d60098dc9aed5fd913b3f1100
Yan Pashkovsky <Yanpas@users.noreply.github.com>
2018-07-13 11:42:23 +0300

    fix c array length (#28)
    fix #27

--- problems/bubble-sort/bubble-sort.c
+++ problems/bubble-sort/bubble-sort.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-void bubblesort(int *v){
+void bubblesort(int *v, size_t size){
 
     int i,j,aux;
-
-    int size = sizeof(v) / sizeof(v[0]); // Number of indexes in the V array.
 
     for (i=0; i < size-1; i++) {
         for(j=i+1; j < size; j++) {
