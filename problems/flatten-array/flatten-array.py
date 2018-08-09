9ba54fc9d7c9665e93cd0c2dcb4d065aa0122ef1
Z <onianidavid@gmail.com>
2018-08-09 03:00:52 -0500

    Added the string approach (#34)
    This is the same approach as in `array-sum` problem with the only difference in what the functions return. Also, in contrast to the first approach (the one you had), this approach makes "in-place" changes of the list in lieu of creating the new instance of the class.

--- problems/flatten-array/flatten-array.py
+++ problems/flatten-array/flatten-array.py
@@ -10,3 +10,21 @@ def flatten(arr):
 
 print flatten([1, 2, [3, [4], 5, 6], 7]) # [1, 2, 3, 4, 5, 6, 7]
 
+
+# Another way of doing it using string conversion
+# Note that it returns the same 'arr' instance without creating a new list
+def flatten(arr):
+    """
+    1. Converts the array into the string and removes characters - '[', ']'
+    2. Splits on commas (',') to get the list of integers
+    3. Removes the trailing whitespaces (in python, elements of the list are separated by comma + space)
+    4. Modifies the elements of the initial array
+    """
+    arr = str(arr)
+    arr = arr.replace('[', '').replace(']', '').split(',')
+    for i in range(len(arr)):
+        if arr[i].strip().isdigit():
+            arr[i] = int(arr[i])
+    return arr
+
+print(flatten([1, 2, [3, [4], 5, 6], 7, [8, 9, [10, 11, [12, [13, 14]]]]])) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
