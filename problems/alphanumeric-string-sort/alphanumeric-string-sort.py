6bd1ce3eec9812d81a764bb253ea1732efa84fbf
Z <onianidavid@gmail.com>
2018-08-09 11:50:42 -0500

    Rename argument

--- problems/alphanumeric-string-sort/alphanumeric-string-sort.py
+++ problems/alphanumeric-string-sort/alphanumeric-string-sort.py
@@ -1,6 +1,6 @@
-def alphanum_sort(string):
+def alphanum_sort(alphanum_string):
     char_string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0246813579"
-    return ''.join(sorted(string, key=char_string.index))
+    return ''.join(sorted(alphanum_string, key=char_string.index))
 
 print(alphanum_sort("Sorting0123456789"))             # ginortS0246813579
 print(alphanum_sort("foobar1237348421"))              # abfoor2244811337
