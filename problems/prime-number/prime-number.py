ec50ea3905a88af1978358d065cd3024326a529e
Z <onianidavid@gmail.com>
2018-08-08 19:37:33 -0500

    `elif n == 2` should be on top
    `elif n == 2` should be on top since it's the only even prime and the new optimization would return `False` before reaching the `elif n == 2`

--- problems/prime-number/prime-number.py
+++ problems/prime-number/prime-number.py
@@ -3,11 +3,11 @@ from math import sqrt
 def is_prime(n):
     if n <= 1:
         return False
+    elif n == 2:
+        return True 
     # To understand the statement below, please visit https://github.com/mre/the-coding-interview/pull/33   
     elif n % 6 not in [1, 5]:
         return False
-    elif n == 2:
-        return True
     for i in range(3, int(sqrt(n))+1, 2): # change range to xrange for python2
         if n % i == 0:
             return False
