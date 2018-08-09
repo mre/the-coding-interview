b2a97c7b843e3dc2dc5b94ffed40139d4be27316
Z <onianidavid@gmail.com>
2018-08-08 19:30:08 -0500

    Added the link for the primes' search optimization

--- problems/prime-number/prime-number.py
+++ problems/prime-number/prime-number.py
@@ -3,13 +3,14 @@ from math import sqrt
 def is_prime(n):
     if n <= 1:
         return False
+    # To understand the statement below, please visit https://github.com/mre/the-coding-interview/pull/33   
     elif n % 6 not in [1, 5]:
         return False
     elif n == 2:
         return True
     elif n % 2 == 0:
         return False
-    for i in range(3, int(sqrt(n))+1, 2): # change xrange to range for Python3
+    for i in range(3, int(sqrt(n))+1, 2): # change range to xrange for python2
         if n % i == 0:
             return False
     return True
