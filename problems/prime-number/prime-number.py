78d2e811051e1b52c6e5915e7c4d80134d4a7cbb
Z <onianidavid@gmail.com>
2018-08-09 02:59:40 -0500

    Optimize prime-number check for Python (#33)
    * Update prime-number.py
    
    * Added the link for the primes' search optimization
    
    * Update prime-number.py
    
    It cannot be even since the remainder on division by 6 is either 1 or 5.
    
    * `elif n == 2` should be on top
    
    `elif n == 2` should be on top since it's the only even prime and the new optimization would return `False` before reaching the `elif n == 2`
    
    * 3 needs to be above too
    
    2, 3 are exception in this case (for k > 0 the smallest prime we can get is 5 [6 * 1 - 1] )

--- problems/prime-number/prime-number.py
+++ problems/prime-number/prime-number.py
@@ -3,11 +3,14 @@ from math import sqrt
 def is_prime(n):
     if n <= 1:
         return False
-    elif n == 2:
-        return True
-    elif n % 2 == 0:
+    elif n in [2, 3]:
+        return True 
+    # To understand the statement below, please visit https://github.com/mre/the-coding-interview/pull/33   
+    elif n % 6 not in [1, 5]:
         return False
-    for i in xrange(3, int(sqrt(n))+1, 2):
+    for i in range(3, int(sqrt(n))+1, 2): # change range to xrange for python2
         if n % i == 0:
             return False
     return True
+
+print(is_prime(13))
