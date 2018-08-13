3275827ef5578142e07747f9feacc4f47fc22006
Z <onianidavid@gmail.com>
2018-08-13 10:03:08 -0500

    Update factorial.py

--- problems/factorial/factorial.py
+++ problems/factorial/factorial.py
@@ -1,5 +1,17 @@
+# Recursive factorial
 def fac(n):
     return 1 if n == 1 else n * fac(n-1)
 
-print fac(3)
-print fac(33)
+print(fac(3))  # 6
+print(fac(33)) # 8683317618811886495518194401280000000
+
+# Iterative factorial
+def fac(n):
+    res = i = 1
+    while i <= n:
+        res *= i
+        i += 1
+    return res
+
+print(fac(3))  # 6
+print(fac(33)) # 8683317618811886495518194401280000000
