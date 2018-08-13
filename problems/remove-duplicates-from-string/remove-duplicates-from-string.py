dc61eac815c60f089ad9d359cb641521e1554245
Z <onianidavid@gmail.com>
2018-08-13 09:57:19 -0500

    Update remove-duplicates-from-string.py

--- problems/remove-duplicates-from-string/remove-duplicates-from-string.py
+++ problems/remove-duplicates-from-string/remove-duplicates-from-string.py
@@ -1,3 +1,4 @@
+# Python 2
 from string import lower
 
 def remove_duplicates(string):
@@ -7,4 +8,28 @@ def remove_duplicates(string):
         seen.append(ch)
   return "".join(seen)
 
-print remove_duplicates("tree traversal")
+print remove_duplicates("tree traversal") # tre avsl
+
+# Python 3
+def remove_dupes(string):
+    seen = []
+    for ch in string.lower():
+        if ch not in seen:
+            seen.append(ch)
+    return "".join(seen)
+
+print(remove_dupes("tree traversal")) # tre avsl
+
+# This is O(n^2) but one-liner
+def remove_dupes(string):
+    return ''.join(sorted(set(string), key=string.index))
+
+print(remove_dupes("tree traversal")) # tre avsl
+
+# Another approach using OrderedDict
+from collections import OrderedDict
+
+def remove_dupes(string):
+    return "".join(OrderedDict.fromkeys(string)) 
+
+print(remove_dupes("tree traversal")) # tre avsl
