113481aed39498fcc7b2a2eae0773682bb864135
Richard Fussenegger <fleshgrinder@users.noreply.github.com>
2018-08-16 19:15:44 +0200

    Rust: array sum
    This Rust solution allows arbitrary input dimensions.

---
+++ problems/array-sum/array-sum.rs
@@ -0,0 +1,38 @@
+#[derive(Clone)]
+enum Either {
+    List(Vec<Either>),
+    Int(u32),
+}
+
+fn array_sum(input: Vec<Either>) -> u32 {
+    if input.is_empty() {
+        0
+    } else {
+        let mut result = 0;
+        let mut stack = vec![input];
+        while let Some(next) = stack.pop() {
+            for item in next.iter() {
+                match item {
+                    Either::List(it) => stack.push(it.to_vec()),
+                    Either::Int(it) => result += it,
+                }
+            }
+        }
+        result
+    }
+}
+
+fn main() {
+    assert_eq!(
+        15,
+        array_sum(vec![
+            Either::Int(1),
+            Either::Int(2),
+            Either::List(vec![
+                Either::Int(3),
+                Either::Int(4),
+                Either::List(vec![Either::Int(5),]),
+            ]),
+        ])
+    );
+}
