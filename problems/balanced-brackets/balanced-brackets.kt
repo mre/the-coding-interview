32142422906e7986313ec5f9a7fcb049f930cc93
Richard Fussenegger <fleshgrinder@users.noreply.github.com>
2018-08-18 17:06:02 +0200

    Added imports and tests

--- problems/balanced-brackets/balanced-brackets.kt
+++ problems/balanced-brackets/balanced-brackets.kt
@@ -1,3 +1,7 @@
+import java.util.Stack
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
 fun balancedBrackets(input: String, brackets: Map<Char, Char> = mapOf(']' to '[', ')' to '(', '}' to '{')) =
     Stack<Char>().apply {
         require(input.all { it in brackets.flatMap { (k, v) -> listOf(k, v) } })
@@ -9,3 +13,10 @@ fun balancedBrackets(input: String, brackets: Map<Char, Char> = mapOf(']' to '['
             }
         }
     }.isEmpty()
+
+
+fun main(args: Array<String>) {
+    assertTrue(balancedBrackets("()[]{}(([])){[()][]}"))
+    assertFalse(balancedBrackets("())[]{}"))
+    assertFalse(balancedBrackets("[(])"))
+}
