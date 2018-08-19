79773ad2a909e0518d90dcb51514b2ff9be37514
Richard Fussenegger <fleshgrinder@users.noreply.github.com>
2018-08-19 09:53:56 +0200

    Added in-place variant

--- problems/bubble-sort/bubble-sort.kt
+++ problems/bubble-sort/bubble-sort.kt
@@ -1,18 +1,19 @@
 import kotlin.test.assertEquals
 
-fun <E : Comparable<E>> List<E>.bubbleSorted(): List<E> =
-    if (size < 2) this else {
-        ArrayList<E>(this).apply {
-            for (i in 0 until size)
-                for (j in (i + 1) until size)
-                    if (this[i] > this[j]) {
-                        val a = get(i)
-                        val b = get(j)
-                        set(i, b)
-                        set(j, a)
-                    }
-        }
+fun <E : Comparable<E>> MutableList<E>.bubbleSort(): MutableList<E> =
+    if (size < 2) this else apply {
+        for (i in 0 until size)
+            for (j in (i + 1) until size)
+                if (this[i] > this[j]) {
+                    val a = get(i)
+                    val b = get(j)
+                    set(i, b)
+                    set(j, a)
+                }
     }
+
+fun <E : Comparable<E>> List<E>.bubbleSorted(): List<E> =
+    ArrayList<E>(this).bubbleSort()
 
 fun main(args: Array<String>) {
     assertEquals(listOf(1), listOf(1).bubbleSorted())
