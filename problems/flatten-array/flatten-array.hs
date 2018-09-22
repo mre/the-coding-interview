2eabaa85818b30a5c457ad311dfadeaa75561eab
Z <onianidavid@gmail.com>
2018-09-21 22:18:18 -0500

    Update flatten-array.hs

--- problems/flatten-array/flatten-array.hs
+++ problems/flatten-array/flatten-array.hs
@@ -1,4 +1,3 @@
-```haskell
 -- This is a classic implementation using Haskell
 
 data UnflattenedArray x = El x | List [UnflattenedArray x] -- creating the nested list / UnflattenedArray datatype
@@ -11,4 +10,3 @@ flatten (List y) = concatMap flatten y
 main = do
     print (flatten (List [El 1, List [El 2, List [El 3, El 4, El 5, El 6], El 7, El 8, El 9], List [El 10]]))
 -- Output: [1,2,3,4,5,6,7,8,9,10]
-```
