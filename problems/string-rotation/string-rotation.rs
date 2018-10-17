a2d604ea2a19bb8ac68a028ba39469aea1fac65a
JoshBrudnak <jobrud314@gmail.com>
2018-10-17 17:38:50 -0400

    Added rust solution to string-rotation

---
+++ problems/string-rotation/string-rotation.rs
@@ -0,0 +1,31 @@
+fn is_rotation(forward: &str, reverse: &str) -> bool {
+    let forward_chars: Vec<char> = forward.chars().collect();
+    let reverse_chars: Vec<char> = reverse.chars().collect();
+    let mut rev_match = true;
+
+    if forward_chars.len() == reverse_chars.len() {
+        let mut i = forward_chars.len() - 1;
+
+        for c in forward_chars {
+            if c != reverse_chars[i] {
+                rev_match = false;
+                break;
+            }
+
+            i = if i > 0 {
+                i - 1
+            } else {
+                break;
+            };
+        }
+    }
+
+    rev_match
+}
+
+fn main() {
+    println!("{}", is_rotation("ABCD", "DCBA"));
+    println!("{}", is_rotation("ABCD", "BCDA"));
+    println!("{}", is_rotation("ABCD", "ACBD"));
+    println!("{}", is_rotation("ABCD", "BCDA"));
+}
