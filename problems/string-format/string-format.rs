f1d51369f5f79ba080849f518a1b38cdf6582ab8
JoshBrudnak <jobrud314@gmail.com>
2018-10-17 17:10:44 -0400

    Add rust solution for string-format

---
+++ problems/string-format/string-format.rs
@@ -0,0 +1,18 @@
+fn format(orig_str: &str, args: Vec<&str>) -> String {
+    let mut new_str = orig_str.to_string();
+
+    for (i, arg) in args.iter().enumerate() {
+        let temp_str = new_str.clone();
+        let parts: Vec<&str> = temp_str
+            .split(format!("{}{}{}", "{", i.to_string(), "}").as_str())
+            .collect();
+
+        new_str = parts.join(arg);
+    }
+
+    new_str
+}
+
+fn main() {
+    println!("{}\n", format("Hello {0} {1}", vec!["Mr.", "X"]));
+}
