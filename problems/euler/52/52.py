555f7f5a6f8f2b3b5f83e02ae623297a1a06eb14
Abhiraj Deshpande <ahdeshpande@users.noreply.github.com>
2018-11-15 10:22:40 -0500

    refactor: Remove empty lines

--- problems/euler/52/52.py
+++ problems/euler/52/52.py
@@ -11,29 +11,20 @@ Find the smallest positive integer, x, such that
 
 def permutedMutables(num):
 	dig_set = set(str(num))
-
-	for i in range(2, 7):
+  for i in range(2, 7):
 		val = num * i
 		val_set = set(str(val))
-
 		if not dig_set == val_set:
 			return False
-
 	return True
 
 def euler52():
-
 	num = 2
-
 	result = False
-
 	while not result:
-
 		result = permutedMutables(num)
-		
 		if not result:
 			num += 1
-
 	return num
 
 print(euler52())
