cf91d78065abf26ec0a17deb7eeb20d1a77e54f7
IrgendNJulian <julian.nohr@uni-ulm.de>
2019-01-25 15:44:10 +0100

    Replace existing canScramble Kotlin solution
    The previous algorithm won't work for words having the same character ascii sum. For example abd and acc would pass the test. Here is the classic sorting and comparing approach.

--- problems/can-scramble/can-scramble.kt
+++ problems/can-scramble/can-scramble.kt
@@ -1,10 +1,5 @@
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
-
-fun String.canScramble(other: String) =
-    length == other.length && chars().sum() == other.chars().sum()
-
-fun main(args: Array<String>) {
-    assertTrue("abc".canScramble("cba"))
-    assertFalse("aab".canScramble("bba"))
-}
+infix fun String.canScramble(other: String) =
+    if (length != other.length)
+        false
+    else
+        toCharArray().sortedArray() contentEquals other.toCharArray().sortedArray()
