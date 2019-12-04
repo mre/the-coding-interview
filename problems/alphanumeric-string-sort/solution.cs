51176977f792f35a05060dd87b640247beefbf32
Fleshgrinder <fleshgrinder@users.noreply.github.com>
2019-12-04 15:14:33 +0100

    Simplified C# solution

--- problems/alphanumeric-string-sort/solution.cs
+++ problems/alphanumeric-string-sort/solution.cs
@@ -7,31 +7,19 @@ namespace ConsoleApp1 {
         static void Main(string[] args) {
             string input = new string(args[0].OrderBy(c => c).ToArray());
 
-            string lower = "";
-            string upper = "";
-            string even = "";
-            string odd = "";
+            string lower = "", upper = "", even = "", odd = "";
             for (int i = 0; i < input.Length; i++) {
                 if (Char.IsLower(input[i])) {
                     lower += input[i];
                 } else if (Char.IsUpper(input[i])) {
                     upper += input[i];
+                } else if (Char.GetNumericValue(input[i]) % 2 == 0) {
+                    even += input[i];
                 } else {
-                    switch (input[i]) {
-                        case '0':
-                        case '2':
-                        case '4':
-                        case '6':
-                        case '8':
-                            even += input[i];
-                            break;
-
-                        default:
-                            odd += input[i];
-                            break;
-                    }
+                    odd += input[i];
                 }
             }
+
             Console.WriteLine(lower + upper + even + odd);
         }
     }
