5570f8f3b58a46e77bf04e3ecaef7ce6ff76e31b
Nir Feldman <nir.feldman@kenshoo.com>
2019-12-30 14:44:59 +0200

    Update shortest-fizz-buzz.java

--- problems/shortest-fizz-buzz/shortest-fizz-buzz.java
+++ problems/shortest-fizz-buzz/shortest-fizz-buzz.java
@@ -4,10 +4,10 @@ class ShortestFizzBuzz {
   for (int x = 1; x <= 100; x++) {
     System.out.println();
     if (x % 3 == 0) {
-      System.out.print("fizz");
+      System.out.print("Fizz");
     }
     if (x % 5 == 0) {
-      System.out.print("buzz");
+      System.out.print("Buzz");
     } if (x % 3 != 0 && x % 5 != 0) {
       System.out.print(x);
     }
