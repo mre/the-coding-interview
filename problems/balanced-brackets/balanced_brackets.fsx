936f567cee4bd70f78817eb7a685cf35db84efcb
gustav-langer <ich.leake.nicht@meine.email.adresse>
2023-10-07 12:47:22 +0200

    Removed unused code

--- problems/balanced-brackets/balanced_brackets.fsx
+++ problems/balanced-brackets/balanced_brackets.fsx
@@ -1,100 +1,87 @@
-module Result =
-    let resultSequence resultList =
-        let folder result state =
-            match state, result with
-            | Error _, _ -> state
-            | Ok listSoFar, Ok value -> Ok(value :: listSoFar)
-            | Ok _, Error e -> Error e
+let resultSequence resultList =
+    let folder result state =
+        match state, result with
+        | Error _, _ -> state
+        | Ok listSoFar, Ok value -> Ok(value :: listSoFar)
+        | Ok _, Error e -> Error e
 
-        List.foldBack folder resultList (Ok [])
+    List.foldBack folder resultList (Ok [])
 
-    let expectSome message option =
-        match option with
-        | Some x -> Ok x
-        | None -> Error message
+type BracketVariant =
+    | Round
+    | Square
+    | Curly
 
-    type ResultBuilder() =
-        member _.Bind(x, f) = Result.bind f x
-        member _.Return(x) = Ok x
+type BracketDirection =
+    | Open
+    | Close
 
-    let result = new ResultBuilder()
+type Bracket =
+    { variant: BracketVariant
+      direction: BracketDirection }
 
-module Brackets =
-    open Result
+let bracketVariant =
+    function
+    | '('
+    | ')' -> Some Round
+    | '['
+    | ']' -> Some Square
+    | '{'
+    | '}' -> Some Curly
+    | _ -> None
 
-    type BracketVariant =
-        | Round
-        | Square
-        | Curly
+let bracketDirection =
+    function
+    | '('
+    | '['
+    | '{' -> Some Open
+    | ')'
+    | ']'
+    | '}' -> Some Close
+    | _ -> None
 
-    type BracketDirection =
-        | Open
-        | Close
+let bracket char =
+    match bracketVariant char, bracketDirection char with
+    | Some v, Some d -> Ok { variant = v; direction = d }
+    | _ -> Error <| sprintf "Unknown character %c" char
 
-    type Bracket =
-        { variant: BracketVariant
-          direction: BracketDirection }
+let readString (str: string) =
+    str.ToCharArray() |> List.ofArray |> List.map bracket |> resultSequence
 
-    let bracketVariant =
-        function
-        | '('
-        | ')' -> Some Round
-        | '['
-        | ']' -> Some Square
-        | '{'
-        | '}' -> Some Curly
-        | _ -> None
+let checkBalance brackets =
+    let folder state bracket =
+        match state, bracket.direction, bracket.variant with
+        | None, _, _ -> None
+        | Some bracketStack, Open, v -> Some <| v :: bracketStack
+        | Some(top :: rest), Close, variant when top = variant -> Some rest
+        | Some _, Close, _ -> None
 
-    let bracketDirection =
-        function
-        | '('
-        | '['
-        | '{' -> Some Open
-        | ')'
-        | ']'
-        | '}' -> Some Close
-        | _ -> None
+    List.fold folder (Some []) brackets
+    |> Option.bind (function
+        | [] -> Some []
+        | _ -> None) // Expect the stack to be empty at the end
+    |> Option.isSome
 
-    let bracket char =
-        match bracketVariant char, bracketDirection char with
-        | Some v, Some d -> Ok { variant = v; direction = d }
-        | _ -> Error <| sprintf "Unknown character %c" char
+let balancedBrackets string =
+    match readString string with
+    | Error error -> Error error
+    | Ok brackets -> Ok(checkBalance brackets)
 
-    let readString (str: string) =
-        str.ToCharArray() |> List.ofArray |> List.map bracket |> resultSequence
+let main () =
+    printfn ""
+    printfn "Enter a string to check for balance:"
+    let string = System.Console.ReadLine()
 
-    let checkBalance brackets =
-        let folder state bracket =
-            match state, bracket.direction, bracket.variant with
-            | None, _, _ -> None
-            | Some bracketStack, Open, v -> Some <| v :: bracketStack
-            | Some(top :: rest), Close, variant when top = variant -> Some rest
-            | Some _, Close, _ -> None
+    match balancedBrackets string with
+    | Error e ->
+        eprintfn "%s" e
+        65
+    | Ok true ->
+        printfn "The brackets are balanced."
+        0
+    | Ok false ->
+        printfn "The brackets are not balanced."
+        0
 
-        List.fold folder (Some []) brackets
-        |> Option.bind (function
-            | [] -> Some []
-            | _ -> None) // Expect the stack to be empty at the end
-        |> Option.isSome
-    
-    let balancedBrackets string =
-        match readString string with
-        | Error error -> Error error
-        | Ok brackets -> Ok (checkBalance brackets)
-
-    let main() =
-        printfn ""
-        printfn "Enter a string to check for balance:"
-        let string = System.Console.ReadLine()
-        match balancedBrackets string with
-        | Error e ->
-            eprintfn "%s" e
-            65
-        | Ok true ->
-            printfn "The brackets are balanced."
-            0
-        | Ok false ->
-            printfn "The brackets are not balanced."
-            0
-
-while true do Brackets.main() |> ignore
+while true do
+    main () |> ignore
